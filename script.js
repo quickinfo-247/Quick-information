c5ab52295af756d1b3b7156249250ff521dcb3e1
quickinfo-247 <quickinformation247@gmail.com>
2026-07-24 01:32:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Quick Information Website Loaded");
+document.addEventListener("DOMContentLoaded", () => {
 
-    const orderButton = document.querySelector("button");
+  // Search
+  const search = document.getElementById("search");
 
-    if (orderButton) {
-        orderButton.addEventListener("click", function () {
-            alert("Welcome to Quick Information!\nOnline Order System Coming Soon.");
-        });
-    }
+  if (search) {
+    search.addEventListener("keyup", function () {
+      let value = this.value.toLowerCase();
+      let cards = document.querySelectorAll(".product-card");
+
+      cards.forEach(card => {
+        let name = card.querySelector("h3").textContent.toLowerCase();
+
+        if (name.includes(value)) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+      });
+    });
+  }
+
+  // WhatsApp Order
+  const buttons = document.querySelectorAll(".product-card button");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", function () {
+
+      let product =
+        this.parentElement.querySelector("h3").textContent;
+
+      let price =
+        this.parentElement.querySelector("p").textContent;
+
+      let message =
+        `Hello Quick Information,%0A%0AI want to order:%0A${product}%0APrice: ${price}`;
+
+      window.open(
+        `https://wa.me/918509727933?text=${message}`,
+        "_blank"
+      );
+
+    });
+  });
+
 });

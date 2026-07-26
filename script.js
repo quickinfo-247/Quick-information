c3b424f62beb851224facfc6e242a3cbe119f165
quickinfo-247 <quickinformation247@gmail.com>
2026-07-26 20:09:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -232,3 +232,28 @@ function openPopup(name, price, image, description){
 function closePopup(){
     document.getElementById("product-popup").style.display = "none";
 }
+// =======================
+// Auto Banner Slider
+// =======================
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+if(slides.length > 0){
+
+setInterval(()=>{
+
+slides[currentSlide].classList.remove("active");
+
+currentSlide++;
+
+if(currentSlide >= slides.length){
+currentSlide = 0;
+}
+
+slides[currentSlide].classList.add("active");
+
+},3000);
+
+}

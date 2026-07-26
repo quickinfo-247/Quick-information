5b4700e2872d2237951e5fb2ee5a5533c0c736f7
quickinfo-247 <quickinformation247@gmail.com>
2026-07-26 20:49:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -285,3 +285,26 @@ setInterval(()=>{
     showSlide(currentSlide);
 
 },3000);
+// ===== Banner Slider =====
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+if (slides.length > 0) {
+
+    setInterval(function () {
+
+        slides[currentSlide].classList.remove("active");
+
+        currentSlide++;
+
+        if (currentSlide >= slides.length) {
+            currentSlide = 0;
+        }
+
+        slides[currentSlide].classList.add("active");
+
+    }, 3000);
+
+}

d612e6d0c8251d808d7f5a037e282050a807b821
quickinfo-247 <quickinformation247@gmail.com>
2026-07-26 20:26:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -257,3 +257,31 @@ slides[currentSlide].classList.add("active");
 },3000);
 
 }
+// ===== Auto Banner Slider =====
+
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide => slide.classList.remove("active"));
+    dots.forEach(dot => dot.classList.remove("active"));
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+
+}
+
+setInterval(()=>{
+
+    currentSlide++;
+
+    if(currentSlide>=slides.length){
+        currentSlide=0;
+    }
+
+    showSlide(currentSlide);
+
+},3000);

76bb49070a7150e0986cf828418d9c976147159a
quickinfo-247 <quickinformation247@gmail.com>
2026-07-27 12:24:15 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,35 @@
+const products = [
+
+{
+    name: "9W Ultra LED Bulb",
+    price: 50,
+    image: "images/bulb9w.jpg",
+    category: "lights",
+    description: "9W Energy Saving LED Bulb"
+},
+
+{
+    name: "Philips AC/DC Bulb",
+    price: 300,
+    image: "images/philips.jpg",
+    category: "lights",
+    description: "Original Philips AC/DC LED Bulb"
+},
+
+{
+    name: "555 Motor",
+    price: 110,
+    image: "images/555motor.jpg",
+    category: "electronics",
+    description: "High Speed DC Motor"
+},
+
+{
+    name: "4 Channel DVR",
+    price: 2950,
+    image: "images/dvr4ch.jpg",
+    category: "cctv",
+    description: "4 Channel CCTV DVR"
+}
+
+];

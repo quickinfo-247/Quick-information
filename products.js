6e838718f110b3aeac9dd97420c9f5f4be7833aa
quickinfo-247 <quickinformation247@gmail.com>
2026-07-27 17:25:16 +0530

    Update products.js

--- products.js
+++ products.js
@@ -17,6 +17,22 @@ const products = [
 },
 
 {
+    name: "Oreva AC/DC Bulb",
+    price: 250,
+    image: "images/oreva.jpg",
+    category: "lights",
+    description: "Rechargeable LED Bulb"
+},
+
+{
+    name: "20W T5 Tube Light",
+    price: 120,
+    image: "images/t5tube.jpg",
+    category: "lights",
+    description: "20W LED Tube Light"
+},
+
+{
     name: "555 Motor",
     price: 110,
     image: "images/555motor.jpg",
@@ -25,12 +41,45 @@ const products = [
 },
 
 {
+    name: "12V 2A Adapter",
+    price: 180,
+    image: "images/adapter12v2a.jpg",
+    category: "cctv",
+    description: "12 Volt 2A Power Adapter"
+},
+
+{
+    name: "12V 5A SMPS",
+    price: 580,
+    image: "images/smps12v5a.jpg",
+    category: "cctv",
+    description: "12 Volt 5A CCTV SMPS"
+},
+
+{
     name: "4 Channel DVR",
     price: 2950,
     image: "images/dvr4ch.jpg",
     category: "cctv",
     description: "4 Channel CCTV DVR"
+},
+
+{
+    name: "19 Inch Monitor",
+    price: 2450,
+    image: "images/monitor19.jpg",
+    category: "cctv",
+    description: "LED Monitor"
+},
+
+{
+    name: "3+1 CCTV Cable",
+    price: 1280,
+    image: "images/cable3plus1.jpg",
+    category: "cctv",
+    description: "Copper CCTV Cable"
 }
 
 ];
+
 export { products };

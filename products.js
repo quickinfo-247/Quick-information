9ba0b463a803b7d6a4ddf578921867362a2912d1
quickinfo-247 <quickinformation247@gmail.com>
2026-07-28 17:15:46 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,73 +1,99 @@
 const products = [
 
 {
-name:"9W Ultra LED Bulb",
-price:50,
-category:"Electrical",
-image:"images/bulb9w.jpg"
+    id:1,
+    name:"9W Ultra LED Bulb",
+    category:"Electrical",
+    price:50,
+    oldPrice:70,
+    image:"images/bulb9w.jpg",
+    phone:"8509727933"
 },
 
 {
-name:"Philips AC DC Bulb",
-price:300,
-category:"Electrical",
-image:"images/philips.jpg"
+    id:2,
+    name:"Philips AC DC Bulb",
+    category:"Electrical",
+    price:300,
+    oldPrice:350,
+    image:"images/philips.jpg",
+    phone:"8509727933"
 },
 
 {
-name:"Oreva AC DC Bulb",
-price:250,
-category:"Electrical",
-image:"images/oreva.jpg"
+    id:3,
+    name:"Oreva AC DC Bulb",
+    category:"Electrical",
+    price:250,
+    oldPrice:290,
+    image:"images/oreva.jpg",
+    phone:"8509727933"
 },
 
 {
-name:"555 DC Motor",
-price:110,
-category:"Electronics",
-image:"images/555motor.jpg"
+    id:4,
+    name:"555 Motor",
+    category:"Electronics",
+    price:110,
+    image:"images/555motor.jpg",
+    phone:"8509727933"
 },
 
 {
-name:"12V 2A Adapter",
-price:180,
-category:"Electronics",
-image:"images/adapter12v2a.jpg"
+    id:5,
+    name:"12V 2A Adapter",
+    category:"Electronics",
+    price:180,
+    image:"images/adapter12v2a.jpg",
+    phone:"8509727933"
 },
 
 {
-name:"12V 5A SMPS",
-price:580,
-category:"Electronics",
-image:"images/smps12v5a.jpg"
+    id:6,
+    name:"12V 5A SMPS",
+    category:"Electronics",
+    price:580,
+    oldPrice:650,
+    image:"images/smps5a.jpg",
+    phone:"8509727933"
 },
 
 {
-name:"4 Channel DVR",
-price:2950,
-category:"CCTV",
-image:"images/dvr4ch.jpg"
+    id:7,
+    name:"4 Channel DVR",
+    category:"CCTV",
+    price:2950,
+    oldPrice:3300,
+    image:"images/dvr4ch.jpg",
+    phone:"8509727933"
 },
 
 {
-name:"19 Inch Monitor",
-price:2450,
-category:"CCTV",
-image:"images/monitor19.jpg"
+    id:8,
+    name:"19 Inch Monitor",
+    category:"CCTV",
+    price:2450,
+    image:"images/monitor19.jpg",
+    phone:"8509727933"
 },
 
 {
-name:"3+1 CCTV Cable",
-price:1280,
-category:"CCTV",
-image:"images/cable3plus1.jpg"
+    id:9,
+    name:"3+1 CCTV Cable",
+    category:"CCTV",
+    price:1280,
+    image:"images/cable3plus1.jpg",
+    phone:"8509727933"
 },
 
 {
-name:"20W T5 tube Light",
-price:1200,
-category:"Solar",
-image:"images/t5tube.jpg"
+    id:10,
+    name:"20W Solar Light",
+    category:"Solar",
+    price:1200,
+    oldPrice:1450,
+    image:"images/solar20w.jpg",
+    phone:"8509727933"
 }
 
 ];
